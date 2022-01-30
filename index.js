275f6b5541f54ee8ac6c279c55cf4cc1c3b19118
paulsalamone <psalamone@gmail.com>
2022-01-30 18:11:09 +0100

    add molecules router

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-// import database
+//BACK END!!!
+const express = require("express");
+const app = express();
+const port = process.env.PGPORT || 3002;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-// establish routes
-// add middlewares
+// allow hiding db info
+require("dotenv").config();
+
+// connect to frontemd
+const cors = require("cors");
+app.use(cors());
+
+// establish routes & middlewares
+const moleculesRouter = require("./routes/moleculesRouter");
 
 //use routes
+app.use("/api/molecules", moleculesRouter);
 
 // listen to server
+app.listen(port, console.log(`Chunks server is listening on port ${port}!`));
